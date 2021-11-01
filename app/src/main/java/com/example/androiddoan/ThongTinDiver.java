5d83d200e7b72fca63d332483dc564f7abd98b5f
Tân <nguyendam554@gmail.com>
2021-11-01 20:50:22 +0700

    Lan Dau Lam Chuyen Do

---
+++ app/src/main/java/com/example/androiddoan/ThongTinDiver.java
@@ -0,0 +1,14 @@
+package com.example.androiddoan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongTinDiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thong_tin_diver);
+    }
+}
