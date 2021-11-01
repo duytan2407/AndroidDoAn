903206441dc664f0a0c36c8b89ab597aa1ec0577
Tân <nguyendam554@gmail.com>
2021-11-01 23:14:11 +0700

    testotp

--- app/src/main/java/com/example/androiddoan/TrangChu.java
+++ app/src/main/java/com/example/androiddoan/TrangChu.java
@@ -3,6 +3,8 @@ package com.example.androiddoan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class TrangChu extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class TrangChu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trang_chu);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setTitleToolbar();
+        getDataIntent();
+    }
+    private void getDataIntent()
+    {
+        String strPhoneNumber= getIntent().getStringExtra("phone number");
+
+
+    }
+
+    private void setTitleToolbar(){
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().setTitle("Trang Chu");
+        }
     }
 }
